e95cae9a17fea2d60e280fdf9700996b2030e8df
RikuRoivainen <bhd722@myy.haaga-helia.fi>
2024-09-01 13:59:27 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addItem = () => {
+    if (inputValue.trim()) {
+      setItems([...items, { key: Math.random().toString(), value: inputValue }]);
+      setInputValue('');
+    }
+  };
+
+  const clearList = () => {
+    setItems([]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <TextInput
+        placeholder='Enter something to list'
+        value={inputValue}
+        onChangeText={setInputValue}
+        style={styles.textInput}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button title='Add' onPress={addItem} />
+        <Button title='Clear' onPress={clearList} />
+      </View>
+
+      <View style={styles.listContainer}>
+        <Text style={styles.header}>Shopping List</Text>
+        <FlatList
+          data={items}
+          renderItem={({ item }) => <Text style={styles.item}>{item.value}</Text>}
+          keyExtractor={item => item.key}
+        />
+      </View>
     </View>
   );
 }
@@ -14,7 +48,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  
+  },
+  textInput: {
+    height: 40,
+    width: '100%',
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 20,
+  },
+  listContainer: {
+    flex: 1, 
+    width: '100%',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    textAlign: 'center',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center', 
   },
 });
